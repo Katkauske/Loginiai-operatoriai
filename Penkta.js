e159c499cc714f78d9da34fa9faced5a476e05fb
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-10-19 10:53:35 +0300

    Pataisyta ir pateikti du variantai
    Jeigu darant funkciją panašiai kaip jūsų varianta, tai funkcijai nereikia paduoti 9 parametrų, užtenka paduoti tris kraštines ir if'o sąlygoje tikrinti jeigu dviejų kvadratas lygus kitos kvadratui ARBA kitų dviejų kvadratas lygu vienos ARBA dar kitų dviejų.
    
    O kas buvo minėta "//Galite pernaudoti funkciją iš praeito skyriaus, kur ieškojote stataus trikampio, 
    //bet tą funkciją pernaudoti kelis kartus su vis skirtingais parametrais ir visus pernaudojimus sujungti viename 
    //if'e su OR operatoriais.", tai ten tiesiog buvo galima naudoti funkciją iš praeitos užduoties kelis kartus ir jai paduodant skirtinga eilės tvarka parametrus. Tą irgi padariau kitoje funkcijoje

--- Penkta.js
+++ Penkta.js
@@ -8,23 +8,55 @@
 let a = 13;
 let b = 11;
 let c = 12;
-console.log(arStatus(a,b,c));
+console.log(arStatusV1(a,b,c));
 
 let sk1 = 31;
 let sk2 = 27;
 let sk3 = 30;
-console.log(arStatus(sk1,sk2,sk3))
+console.log(arStatusV1(sk1,sk2,sk3))
 
 let v = 12;
 let z = 16;
 let x = 20;
-console.log(arStatus(v,z,x))
+console.log(arStatusV1(v,z,x))
+
+
+//------------------testavimai
+console.log("Ss" + arStatusV1(3, 4, 5));
+
+console.log(arStatusV1(2, 5, 3));       // turėtų išvesti false
+console.log(arStatusV1(3, 5, 4));       // turėtų išvesti true
+console.log(arStatusV1(5, 4, 3));       // turėtų išvesti true
+
+console.log(patikrinaArStatusNerikiuotus(2, 5, 3));     // false
+console.log(patikrinaArStatusNerikiuotus(3, 5, 4));     // true
+console.log(patikrinaArStatusNerikiuotus(5, 4, 3));     // true
 
 
 
-function arStatus(a,b,c,sk1,sk2,sk3,v,z,x) {
-    if (((c * c) == (a * a) + (b * b)) || ((sk3 * sk3) == (sk1 * sk1) + (sk2 * sk2)) || ((x * x) == (v * v) + (z * z))) {
-    return true;
+function arStatusV1(a,b,c) {
+    if (((c * c) == (a * a) + (b * b)) || (a * a == b * b + c * c) || (b * b == a * a + c * c)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+/*-------------------- Antro varianto funkcijos ----------------------- */
+function arStatusV2(a,b,c) {
+    if ((c * c) == (a * a) + (b * b)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function patikrinaArStatusNerikiuotus(a, b, c) {
+    if (arStatusV2(a, b, c) || arStatusV2(a, c, b) || arStatusV2(b, c, a)) {       // tiesiog paskutinį parametrą vis paduodu skirtingą raidę, tai taip patikrins tris skirtingus variantus su trim skirtingom įžambinėm (gal kuri nors iš jų bus tikroji)
+        return true;
     }
     else {
         return false;
